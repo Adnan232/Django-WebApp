2403c8ffe62811b944230a18e76aef67debaff32
Adnan232 <adnanhabib120@gmail.com>
2021-10-03 13:09:11 +0530

    improved functionality

--- App/urls.py
+++ App/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('list/', views.list_todo_items),
-    path('insert_todo/', views.insert_list_item, name = 'insert_todo_item'),
+urlpatterns =[
+    path('list/',views.list_todo_items),
+    path('insert_todo/',views.insert_todo_item,name='insert_todo_item'),
+    path('delete_todo/<int:todo_id>/',views.delete_todo_item,name='delete_todo_item'),
 ]
--- App/views.py
+++ App/views.py
@@ -4,15 +4,18 @@ from .models import ToDo
 
 # Create your views here.
 
+
 def list_todo_items(request):
-  return render(request, 'todoApp/todoList.html')
+    context = {'todo_list' : ToDo.objects.all()}
+    return render(request, 'todoApp/todoList.html',context)
 
-def insert_list_item(request:HttpResponse):
-  # return render(request, 'todoApp/todoList.html')
-  todo = ToDo(content = request.POST['content'])
-  
-  todo.save()
-  return redirect('/App/list/')
 
-def App_home(request):
-  return HttpResponse('From App_Home')
+def insert_todo_item(request: HttpRequest):
+    todo = ToDo(content=request.POST['content'])
+    todo.save()
+    return redirect('/App/list/')
+
+def delete_todo_item(request,todo_id):
+    todo_to_delete = ToDo.objects.get(id=todo_id)
+    todo_to_delete.delete()
+    return redirect('/App/list/')
